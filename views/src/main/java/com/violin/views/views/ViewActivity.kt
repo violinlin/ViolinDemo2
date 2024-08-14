b59c57c55beefaed1a84978fd9550d72568e1b27
wanghuilin <violinlin@yeah.net>
2024-08-14 21:12:43 +0800

    pag 工具类添加

--- views/src/main/java/com/violin/views/views/ViewActivity.kt
+++ views/src/main/java/com/violin/views/views/ViewActivity.kt
@@ -1,15 +1,24 @@
 package com.violin.views.views
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.violin.views.R
+import org.libpag.PAGFile
+import org.libpag.PAGView
 
 class ViewActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
+
+        val pagview = findViewById<PAGView>(R.id.pagview)
+        pagview.composition = PAGFile.Load(assets, "anima_lucky_gift_bg.pag")
+        pagview.setRepeatCount(-1)
+        pagview.play()
 
     }
 
